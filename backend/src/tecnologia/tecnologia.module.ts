34d6ab5281440e2b9a4987a79d6d2f73567cc60b
JonasBrazil <jonas-navarro@hotmail.com>
2025-03-19 21:14:30 -0300

    feat(backend): api de tecnologia e projetos

---
+++ backend/src/tecnologia/tecnologia.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TecnologiaController } from './tecnologia.controller';
+import { DbModule } from 'src/db/db.module';
+import { TecnologiaPrisma } from './tecnologia.prisma';
+
+@Module({
+  controllers: [TecnologiaController],
+  providers: [TecnologiaPrisma],
+  imports: [DbModule],
+})
+export class TecnologiaModule {}
